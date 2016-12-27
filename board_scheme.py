246a3f02e9a0d5d32b7711877068e123ec141a51
Federico Teotini <templarblack.fed03@gmail.com>
2016-12-27 23:14:38 +0100

    We can print the board now

--- board_scheme.py
+++ board_scheme.py
@@ -1,76 +1,108 @@
+from terminaltables import SingleTable
+
+HORIZONTAL_DIRECTION = {
+    'key_suffix': 'h',
+    'cell_index': 1,
+    'first_pass': 'rows',
+    'second_pass': 'cols'
+}
+
+VERTICAL_DIRECTION = {
+    'key_suffix': 'v',
+    'cell_index': 0,
+    'first_pass': 'cols',
+    'second_pass': 'rows'
+}
+
+
 class BoardScheme:
-    HORIZONTAL_DIRECTION = {
-        'key_suffix': 'h',
-        'cell_index': 1,
-        'first_pass': 'rows',
-        'second_pass': 'cols'
-    }
-
-    VERTICAL_DIRECTION = {
-        'key_suffix': 'v',
-        'cell_index': 0,
-        'first_pass': 'cols',
-        'second_pass': 'rows'
-    }
-
     def __init__(self, cols, rows, blocks):
         self.cols = cols
         self.rows = rows
         self.blocks = blocks
+        self._horizontal_words = self._generate_words(HORIZONTAL_DIRECTION)
+        self._vertical_words = self._generate_words(VERTICAL_DIRECTION)
+        self._definition_indexes = self._generate_definition_indexes()
 
-    def get_horizontal_words(self):
-        return self.get_words(self.HORIZONTAL_DIRECTION)
+        self.intersections = self._generate_intersections()
 
-    def get_vertical_words(self):
-        return self.get_words(self.VERTICAL_DIRECTION)
+    def _generate_definition_indexes(self):
+        first_letters = list(set(self._horizontal_words.keys()).union(self._vertical_words))
+        first_letters.sort(key=lambda x: x[1])  # sort on secondary key
+        first_letters.sort(key=lambda x: x[0])  # then sort on primary
 
-    def get_intersections(self):
+        return first_letters
+
+    def _generate_intersections(self):
         intersections = set()
-        for h_word in self.get_horizontal_words().values():
-            for v_word in self.get_vertical_words().values():
+        for h_word in self._horizontal_words.values():
+            for v_word in self._vertical_words.values():
                 intersections.update(set(h_word).intersection(v_word))
 
         return intersections
 
-    def get_words(self, direction):
+    def _generate_words(self, direction):
         words = {}
-        for first_coord in range(1, getattr(self, direction['first_pass']) + 1):
+        for x in range(1, getattr(self, direction['first_pass']) + 1):
             last_block = (0, 0)
 
-            for second_coord in range(1, getattr(self, direction['second_pass']) + 1):
-                current_cell = self.get_current_cell(first_coord, second_coord, direction)
+            for y in range(1, getattr(self, direction['second_pass']) + 1):
+                current_cell = _get_current_cell(x, y, direction)
 
                 if current_cell in self.blocks:
 
-                    if self.block_distance(current_cell, last_block, direction) > 2:
-                        key = str(len(words) + 1) + direction['key_suffix']
-                        words[key] = self.generate_word(direction, first_coord, second_coord, last_block)
+                    if _block_distance(current_cell, last_block, direction) > 2:
+                        word = _generate_word(direction, x, y, last_block)
+                        words[word[0]] = word
 
                     last_block = current_cell
-                elif second_coord == getattr(self, direction['second_pass']) \
-                        and self.block_distance(current_cell, last_block, direction) > 1:
-                    key = str(len(words) + 1) + direction['key_suffix']
-                    words[key] = self.generate_word(direction, first_coord, second_coord, last_block)
+                elif y == getattr(self, direction['second_pass']) \
+                        and _block_distance(current_cell, last_block, direction) > 1:
+                    word = _generate_word(direction, x, y, last_block)
+                    key = word[0]
+                    words[key] = word
 
                     words[key].append(current_cell)
 
         return words
 
-    def generate_word(self, direction, fixed_coord, variable_coord, last_block):
-        last_block_index = last_block[direction['cell_index']]
-        words = [(fixed_coord, index) for index in range(last_block_index + 1, variable_coord)]
+    def __str__(self):
+        mapping = {cell: index + 1 for (index, cell) in enumerate(self._definition_indexes)}
+        mapping.update({cell: '#' for cell in self.blocks})
 
-        if direction != self.HORIZONTAL_DIRECTION:
-            words = list(map(lambda word: word[::-1], words))
+        data = []
+        for row in range(1, self.rows + 1):
+            row_data = []
+            for col in range(1, self.cols + 1):
+                cell = (row, col)
+                if cell in mapping:
+                    row_data.append(mapping[cell])
+                else:
+                    row_data.append(' ')
 
-        return words
+            data.append(row_data)
 
-    def get_current_cell(self, first_coord, second_coord, direction):
-        if direction == self.HORIZONTAL_DIRECTION:
-            return first_coord, second_coord
-        else:
-            return second_coord, first_coord
+        board = SingleTable(data)
+        board.inner_row_border = True
+        return board.table
 
-    @staticmethod
-    def block_distance(block1, block2, direction):
-        return abs(block1[direction['cell_index']] - block2[direction['cell_index']])
+
+def _generate_word(direction, fixed_coord, variable_coord, last_block):
+    last_block_index = last_block[direction['cell_index']]
+    words = [(fixed_coord, index) for index in range(last_block_index + 1, variable_coord)]
+
+    if direction != HORIZONTAL_DIRECTION:
+        words = list(map(lambda word: word[::-1], words))
+
+    return words
+
+
+def _block_distance(block1, block2, direction):
+    return abs(block1[direction['cell_index']] - block2[direction['cell_index']])
+
+
+def _get_current_cell(x, y, direction):
+    if direction == HORIZONTAL_DIRECTION:
+        return x, y
+    else:
+        return y, x
