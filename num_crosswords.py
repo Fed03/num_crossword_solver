899879425f29e0bc7f094cea0b5225e0b2aadd17
Federico Teotini <templarblack.fed03@gmail.com>
2016-12-27 23:15:46 +0100

    First attempts to get input values

--- num_crosswords.py
+++ num_crosswords.py
@@ -1,14 +1,77 @@
+import re
 from board_scheme import BoardScheme
 
-board = BoardScheme(rows=5, cols=7, blocks=[
-    (1, 3),
-    (2, 5), (2, 7),
-    (3, 3), (3, 4),
-    (4, 2), (4, 6),
-    (5, 1), (5, 7)
-])
 
-h = board.get_horizontal_words()
-v = board.get_vertical_words()
+# board = BoardScheme(rows=5, cols=7, blocks=[
+#     (1, 3),
+#     (2, 5), (2, 7),
+#     (3, 3), (3, 4),
+#     (4, 2), (4, 6),
+#     (5, 1), (5, 7)
+# ])
+#
 
-print(v)
+
+def board_size():
+    question = 'Board dimension, specified as `rows, cols`\n'
+    size = input('%s' % question)
+    while not re.fullmatch('\d+,\s?\d+', size):
+        print('Incorrect format, retry')
+        size = input(question)
+    return map(int, re.split(',\s?', size))
+
+
+def board_blocks(row_limit, col_limit):
+    question = 'List of blocks, one block at time, specified as `row_index, col_index`.'
+    stop_word = 'stop'
+    print(question, 'If done write `%s`' % stop_word)
+
+    blocks = []
+
+    block = input()
+    while str(block).lower() != ('%s' % stop_word):
+        if re.fullmatch('\d+,\s?\d+', block):
+            block = tuple(map(int, re.split(',\s?', block)))
+            if not (1 <= block[0] <= row_limit and 1 <= block[1] <= col_limit):
+                print('You have exceeded the board limits')
+            else:
+                blocks.append(block)
+        else:
+            print('Incorrect format, retry')
+
+        block = input()
+
+    return blocks
+
+
+def get_domain():
+    domain = __get_domain__()
+    if not 0 < domain[0] < domain[1]:
+        print('Lower bound must me greater than 0')
+        return get_domain()
+
+    return domain
+
+
+def __get_domain__():
+    question = 'Insert the variables domain, specified as `r-s`\n'
+    domain = input('%s' % question)
+    while not re.fullmatch('\d+-\d+', domain):
+        print('Incorrect format, retry')
+        domain = input(question)
+
+    return list(map(int, domain.split('-')))
+
+
+def run():
+    print('Numeric Crosswords Solver\n')
+
+    rows, cols = board_size()
+    blocks = board_blocks(rows, cols)
+
+    board = BoardScheme(cols, rows, blocks)
+    print(board)
+
+    domain = get_domain()
+
+    return
