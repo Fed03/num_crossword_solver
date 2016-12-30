a9cf5e9bd51f39160d7b24003588e44560e5252c
Federico Teotini <templarblack.fed03@gmail.com>
2016-12-31 00:33:57 +0100

    Model initialization with constraint on vars corresponding to blocks

--- crossword_problem.py
+++ crossword_problem.py
@@ -5,3 +5,7 @@ class CrosswordsProblem:
     def __init__(self, board, domain):
         self._board = board
         self._vars = NJ.Matrix(board.rows, board.cols, domain[0], domain[1])
+        self._model = self._build_model()
+
+    def _build_model(self):
+        return NJ.Model([self._vars[row - 1][col - 1] == 0 for (row, col) in self._board.blocks])
